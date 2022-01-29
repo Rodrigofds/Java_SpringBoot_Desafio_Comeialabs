41da5168ddbe953939601b2f2fac983f2adb9093
Rodrigo Ferreira <rodrigoldiscool@gmail.com>
2022-01-29 12:09:45 -0300

    Commit inicial

---
+++ src/test/java/br/com/desafiocomeialabs/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafiocomeialabs.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
